5cc089336e45ef9ee8c19cd5212ea2bca8ec900f
leknoppix <pascal@mirandais.fr>
2014-02-16 19:04:07 +0100

    Ajout du numéro provisoire du bon de commande

---
+++ app/Controller/NumberordersController.php
@@ -0,0 +1,11 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Orderforms Controller
+ *
+ * @property Orderform $Orderform
+ * @property PaginatorComponent $Paginator
+ */
+class NumberordersController extends AppController {
+
+}

4f22963c57ab70f34d15121fcb825d8de8810baa
Pascal Canadas <pascal@mirandais.fr>
2014-02-24 22:03:37 +0100

    correction bug grace à Ch0c4

--- app/webroot/js/jquery.cake.clone.js
+++ app/webroot/js/jquery.cake.clone.js
@@ -5,6 +5,7 @@
 		return this.each(function()
 		{
 			options.nb_copy_initial++;
+
 			$(this).before( 
 				$('.'+options.classclone+options.valeur_initiale)
 				.clone(true,true)
@@ -14,14 +15,16 @@
 	        
 	        $('.'+options.classclone+options.nb_copy_initial+" input").each(function()
 	        {
-	        	$(this).attr("name", $(this).attr("name")
+	        	$(this).attr("name", $(this)
+	        		.attr("name")
 	        		.replace('['+options.controller+'][0]', '['+options.controller+']['+options.nb_copy_initial+']')
 	        	);
-	        	$(this).attr("id", $(this).attr("id")
+	        	$(this).attr("id", $(this)
+	        		.attr("id")
 	        		.replace(options.controller+0, options.controller+options.nb_copy_initial)
 	        	);
 	        });
 		});
 	}
-	
+
 })(jQuery);

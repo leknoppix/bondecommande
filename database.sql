743a8741c2169e23a241d56b8f84cbf7e08e02f2
Pascal Canadas <pascal@mirandais.fr>
2014-03-19 15:02:04 +0100

    Update de la database

--- database.sql
+++ database.sql
@@ -45,7 +45,28 @@ CREATE TABLE IF NOT EXISTS `customers` (
 -- Contenu de la table `customers`
 --
 
+
 -- --------------------------------------------------------
+
+
+--
+-- Structure de la table `numberorders`
+--
+
+CREATE TABLE IF NOT EXISTS `numberorders` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `year` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
+  `num` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `numberorders`
+--
+
+
+-- --------------------------------------------------------
+
 
 --
 -- Structure de la table `orderforms`
@@ -59,33 +80,13 @@ CREATE TABLE IF NOT EXISTS `orderforms` (
   `date` date NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `service_id` int(255) NOT NULL,
-  `etat` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+  `datelivraison` date NOT NULL,
+  `lieulivraison` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Contenu de la table `orderforms`
---
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `orderforms_products`
---
-
-CREATE TABLE IF NOT EXISTS `orderforms_products` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `orderform_id` int(255) NOT NULL,
-  `product_id` int(255) NOT NULL,
-  `amount` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `price` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `tva` float(12,4) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
---
--- Contenu de la table `orderforms_products`
 --
 
 
@@ -99,6 +100,10 @@ CREATE TABLE IF NOT EXISTS `products` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `amout` float NOT NULL,
+  `price` float NOT NULL,
+  `tva` float(12,2) NOT NULL,
+  `orderform_id` int(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -154,30 +159,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `name`, `nom`, `prenom`, `username`, `password`, `mail`, `telephone_fixe`, `telephone_mobile`, `adresse_postale`, `code_postal`, `ville`, `signature`) VALUES
 (1, 'Administrateur', 'admin', 'admin', 'admin', 'f14362cd37c3bc78f6ebb0d78547557fa924fb82', 'admin@admin.fr', '0000000000', '0000000000', 'ah', '0000', 'ah', 'coucou');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `numberorders`
---
-
-CREATE TABLE IF NOT EXISTS `numberorders` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `year` int(4) NOT NULL,
-  `num` int(6) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
---
--- Contenu de la table `numberorders`
---
-
-INSERT INTO  `numberorders` (`id` , `year` , `num` ) VALUES
-(NULL ,  '2014',  '000000' );
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

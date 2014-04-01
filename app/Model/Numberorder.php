dafa1e8ee4322cde033777327c6ea809a0188870
Nicolas Ramy <nicolas.ramy@gmail.com>
2014-04-01 20:06:39 +0200

    Update Numberorder.php

--- app/Model/Numberorder.php
+++ app/Model/Numberorder.php
@@ -5,14 +5,11 @@ App::uses('AppModel', 'Model');
  *
  */
 class Numberorder extends AppModel {
-
-	public function afterFind($results, $primary = false) {
-		foreach ($results as $orderIndex => $order) {
-			$year = $order['Numberorder']['year'];
-			$number = $order['Numberorder']['num'];
-			$results[$orderIndex]['Numberorder']['num'] = str_pad($number, 6, '0', STR_PAD_LEFT);
-			$results[$orderIndex]['Numberorder']['newnum'] = $year . '-' . str_pad($number, 6, '0', STR_PAD_LEFT);
+	
+	public function beforeSave($options = array()) {
+		if (!empty($this->data['Numberorder']['year']) && !empty($this->data['Numberorder']['num'])) {
+			$this->data['Numberorder']['newnum'] = $year . '-' . str_pad($number, 6, '0', STR_PAD_LEFT);
 		}
-		return $results;
-	}
+		return true;
+	}	
 }
